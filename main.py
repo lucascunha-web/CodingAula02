df50e239b0ec78319d1b6150938e0460bff9910e
lucascunha-web <azlucascu@gmail.com>
2026-09-04 20:14:09 -0300

    Primeiro Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+nome = input("Nome do aluno: ")
+
+nota1 = float(input("Digite a nota 1: "))
+nota2 = float(input("Digite a nota 2: "))
+
+media = (nota1 + nota2) / 2
+
+print("Aluno: ", nome)
+print("Média: ", media)
