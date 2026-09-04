652befdfae79d4a09c812a23aac165658f98cdbc
lucascunha-web <azlucascu@gmail.com>
2026-09-04 20:39:41 -0300

    Segundo Commit

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ nome = input("Nome do aluno: ")
 
 nota1 = float(input("Digite a nota 1: "))
 nota2 = float(input("Digite a nota 2: "))
+nota3 = float(input("Digite a nota 3: "))
 
-media = (nota1 + nota2) / 2
+media = (nota1 + nota2 + nota3)  / 3
 
 print("Aluno: ", nome)
 print("Média: ", media)
